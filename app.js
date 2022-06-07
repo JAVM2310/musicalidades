f4d1507d6aa3167a7bedc6e6855af1bfa8e592e4
JAVM2310 <juan.vazquez1611@gmail.com>
2022-06-07 19:58:31 -0300

    arreglados links y cosas varias -J

--- app.js
+++ app.js
@@ -3,23 +3,23 @@ const app = express()
 const path = require("path")
 
 app.get("/", (req, res)=>{
-    res.sendFile(path.join(__dirname, "/views/index.html"))
+    res.sendFile(path.join(__dirname, "./views/index.html"))
 });
 
 app.get("/login", (req, res)=>{
-    res.sendFile(path.join(__dirname, "/views/login.html"))
+    res.sendFile(path.join(__dirname, "./views/login.html"))
 });
 
 app.get("/productCart", (req, res)=>{
-    res.sendFile(path.join(__dirname, "/views/productCart.html"))
+    res.sendFile(path.join(__dirname, "./views/productCart.html"))
 });
 
 app.get("/productDetail", (req, res)=>{
-    res.sendFile(path.join(__dirname, "/views/productDetail.html"))
+    res.sendFile(path.join(__dirname, "./views/productDetail.html"))
 });
 
 app.get("/register", (req, res)=>{
-    res.sendFile(path.join(__dirname, "/views/register.html"))
+    res.sendFile(path.join(__dirname, "./views/register.html"))
 });
 
 app.use(express.static("public"));
