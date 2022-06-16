0ed715cb6741697ed9a144168991ed83fdf1d9ef
JAVM2310 <juan.vazquez1611@gmail.com>
2022-06-16 19:12:14 -0300

    fix

--- src/app.js
+++ src/app.js
@@ -13,30 +13,6 @@ const rutasIndex = require('./routes/indexRoute');
 const rutastienda = require('./routes/tiendaRoute');
 const rutasUsers = require('./routes/usersRoute');
 
-
-/*
-app.get("/", (req, res)=>{
-    res.sendFile(path.join(__dirname, "./views/index.html"))
-});
-
-
-app.get("/login", (req, res)=>{
-    res.sendFile(path.join(__dirname, "./views/login.html"))
-});
-
-app.get("/productCart", (req, res)=>{
-    res.sendFile(path.join(__dirname, "./views/productCart.html"))
-});
-
-app.get("/productDetail", (req, res)=>{
-    res.sendFile(path.join(__dirname, "./views/productDetail.html"))
-});
-
-app.get("/register", (req, res)=>{
-    res.sendFile(path.join(__dirname, "./views/register.html"))
-});*/
-
-
 app.use('/', rutasIndex);
 app.use('/', rutasUsers);
 app.use('/tienda', rutastienda);
