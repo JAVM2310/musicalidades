342499e64248825296c411e87352980d96517fbe
Guadalupe Flores Cottet <Florescottetg@sigen.net>
2022-07-25 19:07:51 -0300

    registro de usuarios

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const fs = require("fs");
+let bcrypt = require('bcryptjs');
+
+const usersFilePath = path.join(__dirname, '../database/users.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 let titulo = "";
 
@@ -14,6 +19,45 @@ const controller = {
     register: (req, res) => {
         let titulo = "Registro"
         res.render('./users/register', {titulo: titulo});
+    },
+
+    registered: (req, res) => {
+        const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8')); 
+        let titulo = "Login";
+
+        let user = req.body;
+        console.log(user);
+            
+        let lastUser = users.slice(-1)[0];
+        let lastIdUser = lastUser.id + 1;
+        let nombreImagen = '';
+        if(req.file != undefined){
+            nombreImagen = '/users/' + req.file.filename;
+        }else{
+            nombreImagen = '/users/default.jpg';
+        }
+        console.log(nombreImagen);
+            
+            let newUser = {
+                id: lastIdUser,
+                nombre: req.body.nombre,
+                apellido: req.body.apellido,
+                password: bcrypt.hashSync(req.body.password, 10),
+                passwordRepetida: bcrypt.hashSync(req.body.passwordRepetida, 10),
+                pais: req.body.pais,
+                provincia: req.body.provincia,
+                ciudad: req.body.ciudad,
+                codigo: req.body.codigo,
+                fechaNac: req.body.fechaNac,
+                avatar: nombreImagen,
+            }
+        //console.log(newUser);
+            
+        users.push(newUser);
+        fs.writeFileSync(usersFilePath, JSON.stringify(users, null, " "))
+            
+        res.redirect("/login");
+        //res.send('hola');
     }
 };
 
--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const multer = require('multer');
+
+/************************** MULTER **************************/
+
+const storage = multer.diskStorage ({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '../../public/img/users'));
+    },
+    filename: (req, file, cb) => {
+        const nameImgUser = 'user-' + Date.now() + path.extname(file.originalname);
+        cb(null, nameImgUser);
+    }
+});
+
+const upload = multer({storage});
 
 /* CON ARCHIVO CONTROLLER*/
 
 const usersController = require ('../controllers/usersController.js');
 
 router.get('/login', usersController.login);
-router.get('/register', usersController.register); 
+router.get('/register', usersController.register);
+router.post('/register', upload.single('avatar'), usersController.registered); 
 
 
 module.exports = router;
